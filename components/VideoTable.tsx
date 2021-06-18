4ce646a30844b714bce13907f4204995ab6d24d5
Markus <markuslim24@gmail.com>
2021-06-18 17:20:52 +0800

    added styling to profilePage

--- components/VideoTable.tsx
+++ components/VideoTable.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 
 import { Typography } from "@material-ui/core";
-import { makeStyles } from "@material-ui/core/styles";
 import Table from "@material-ui/core/Table";
 import TableBody from "@material-ui/core/TableBody";
 import TableCell from "@material-ui/core/TableCell";
@@ -9,30 +8,40 @@ import TableContainer from "@material-ui/core/TableContainer";
 import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
 import Toolbar from "@material-ui/core/Toolbar";
+import Tooltip from "@material-ui/core/Tooltip";
 import Paper from "@material-ui/core/Paper";
+import Button from "@material-ui/core/Button";
+import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles({
   table: {
     minWidth: 650,
   },
+  title: {
+    flex: "1 1 auto",
+  },
 });
 
 function createData(
-  name: string,
-  calories: number,
-  fat: number,
-  carbs: number,
-  protein: number
+  videoName: string,
+  date: string,
+  views: number,
+  comments: number,
+  likes: number
 ) {
-  return { name, calories, fat, carbs, protein };
+  return { videoName, date, views, comments, likes };
 }
 
+const handleUploadButton = () => {
+  console.log("Pls handle this");
+};
+
 const rows = [
-  createData("Video 1", 159, 6.0, 24, 4.0),
-  createData("Video 2", 237, 9.0, 37, 4.3),
-  createData("Video 3", 262, 16.0, 24, 6.0),
-  createData("Video 4", 305, 3.7, 67, 4.3),
-  createData("Video 5", 356, 16.0, 49, 3.9),
+  createData("Video 1", "15/6/2021", 6.0, 24, 4.0),
+  createData("Video 2", "15/6/2021", 9.0, 37, 4.3),
+  createData("Video 3", "15/6/2021", 16.0, 24, 6.0),
+  createData("Video 4", "15/6/2021", 3.7, 67, 4.3),
+  createData("Video 5", "15/6/2021", 16.0, 49, 3.9),
 ];
 
 export default function VideoTable() {
@@ -42,7 +51,18 @@ export default function VideoTable() {
     <>
       <TableContainer component={Paper}>
         <Toolbar>
-          <Typography variant="h6">Videos</Typography>
+          <Typography variant="h6" className={classes.title}>
+            Videos
+          </Typography>
+          <Tooltip title="upload">
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={handleUploadButton}
+            >
+              Upload Video
+            </Button>
+          </Tooltip>
         </Toolbar>
         <Table
           className={classes.table}
@@ -60,14 +80,14 @@ export default function VideoTable() {
           </TableHead>
           <TableBody>
             {rows.map((row) => (
-              <TableRow key={row.name}>
+              <TableRow key={row.videoName}>
                 <TableCell component="th" scope="row">
-                  {row.name}
+                  {row.videoName}
                 </TableCell>
-                <TableCell align="right">{row.calories}</TableCell>
-                <TableCell align="right">{row.fat}</TableCell>
-                <TableCell align="right">{row.carbs}</TableCell>
-                <TableCell align="right">{row.protein}</TableCell>
+                <TableCell align="right">{row.date}</TableCell>
+                <TableCell align="right">{row.views}</TableCell>
+                <TableCell align="right">{row.comments}</TableCell>
+                <TableCell align="right">{row.likes}</TableCell>
               </TableRow>
             ))}
           </TableBody>
--- pages/profile.tsx
+++ pages/profile.tsx
@@ -10,18 +10,47 @@ import Container from "@material-ui/core/Container";
 import Image from "next/image";
 import Button from "@material-ui/core/Button";
 import VideoTable from "../components/VideoTable";
+import { makeStyles } from "@material-ui/core/styles";
+import { Typography } from "@material-ui/core";
+
+const useStyles = makeStyles({
+  profileBar: {
+    margin: "20px 0px",
+    display: "flex",
+    flexDirection: "row",
+    position: "relative",
+  },
+  avatar: {
+    alignSelf: "center",
+    width: "100px",
+    height: "100px",
+  },
+  profileInfo: { marginLeft: "20px", alignSelf: "center" },
+  greeting: {
+    position: "relative",
+    padding: "4px",
+  },
+  editProfileButton: { position: "relative", top: "0px" },
+});
 
 export default function Profile() {
+  const classes = useStyles();
   const user = useSelector(getUser);
 
   return (
     <>
       <Navbar />
       <Container component="main" maxWidth="md">
-        <div>
-          <Avatar src="Avatar.png" />
-          <h1>Hi {user.name}</h1>
-          <Button>Edit Profile</Button>
+        <div className={classes.profileBar}>
+          <Avatar src="Avatar.png" className={classes.avatar} />
+          <div className={classes.profileInfo}>
+            <Typography variant="h4" className={classes.greeting}>
+              Hi {user.name}
+            </Typography>
+            <Button variant="outlined" className={classes.editProfileButton}>
+              Edit Profile
+            </Button>
+          </div>
         </div>
         <div>Statistics</div>
         <VideoTable />
