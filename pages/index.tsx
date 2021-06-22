60c843e91547e6922f5f375a6d2bb88169a8f29a
Markus <markuslim24@gmail.com>
2021-06-23 00:35:40 +0800

    added recommended videos component on homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,23 +3,26 @@ import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getUser, isLoggedIn } from "../store/slice/auth";
 import Navbar from "../components/Navbar";
+import Recommended from "../components/Recommended";
+import { Container } from "@material-ui/core";
 
 //Home page
 export default function Home() {
-  const [message, setMessage] = useState("");
   const isAuthenticated = useSelector(isLoggedIn);
   const user = useSelector(getUser);
   return (
     <>
       <Navbar />
-      {isAuthenticated ? (
-        <>
-          <h1>Hi {user.name}</h1>
-        </>
-      ) : (
-        <h1>User not authenticated</h1>
-      )}
-      <h1>{message}</h1>
+      <Container maxWidth="xl">
+        {isAuthenticated ? (
+          <>
+            <h1>Hi {user.name}</h1>
+          </>
+        ) : (
+          <h1>User not authenticated</h1>
+        )}
+        <Recommended />
+      </Container>
     </>
   );
 }
